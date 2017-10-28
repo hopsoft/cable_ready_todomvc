8ebba041cbdb1f7cb071e2536cf77ce06bb7d498
Nathan Hopkins <natehop@gmail.com>
2017-10-28 06:54:02 -0600

    Create the todo model

---
+++ db/migrate/20171028124846_create_todos.rb
@@ -0,0 +1,9 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.boolean :completed, null: false, default: false
+      t.timestamps
+    end
+  end
+end
