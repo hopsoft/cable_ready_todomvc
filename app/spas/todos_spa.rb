aa65b2835ad99609706febf89aea7547b6ed24ff
Nathan Hopkins <natehop@gmail.com>
2017-11-01 08:19:21 -0600

    Always render the footer

--- app/spas/todos_spa.rb
+++ app/spas/todos_spa.rb
@@ -1,14 +1,16 @@
-require "forwardable"
-
 class TodosSpa
   extend Forwardable
   include CableReady::Broadcaster
   attr_reader :user_id, :params
-  def_delegators :cable_ready, :broadcast
 
   def initialize(user_id, params={})
     @user_id = user_id
     @params = params
+  end
+
+  def broadcast
+    render_footer
+    cable_ready.broadcast
   end
 
   def todo_created(todo)
@@ -17,13 +19,11 @@ class TodosSpa
     todo_operations.remove_css_class     selector: ".main",      name: "hidden"
     todo_operations.remove_css_class     selector: ".footer",    name: "hidden"
     user_operations.set_value            selector: ".new-todo",  value: ""
-    render_footer
   end
 
   def todo_updated(todo)
     todo_operations.replace selector: "##{todo.to_gid_param}",
       html: todo.render_with(partial: "/todos/todo.html", assigns: { filter: params[:filter] })
-    render_footer
   end
 
   def todo_destroyed(todo)
@@ -33,22 +33,18 @@ class TodosSpa
       todo_operations.add_css_class selector: ".main", name: "hidden"
       todo_operations.add_css_class selector: ".footer", name: "hidden"
     end
-
-    render_footer
   end
 
   def todos_shown(todos)
     todos.each do |todo|
       todo_operations.remove_css_class selector: "##{todo.to_gid_param}", name: "hidden"
     end
-    render_footer
   end
 
   def todos_hidden(todos)
     todos.each do |todo|
       todo_operations.add_css_class selector: "##{todo.to_gid_param}", name: "hidden"
     end
-    render_footer
   end
 
   def todo_shown(todo)
