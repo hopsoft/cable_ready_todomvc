7c570206a8e2de9a4571bd924003c8e2996eadc8
Nathan Hopkins <natehop@gmail.com>
2017-11-05 05:12:57 -0700

    Removing more noise

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,15 +1,3 @@
-# == Schema Information
-#
-# Table name: todos
-#
-#  id         :integer          not null, primary key
-#  user_id    :string           not null
-#  title      :string           not null
-#  completed  :boolean          default(FALSE), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Todo < ApplicationRecord
   validates :title, presence: true
   validates :completed, presence: true
