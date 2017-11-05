7fc9a18ce67f4c3ba7770d849774fb052cea270b
Nathan Hopkins <natehop@gmail.com>
2017-11-05 05:25:25 -0700

    Remove incorrect validator

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,5 @@
 class Todo < ApplicationRecord
   validates :title, presence: true
-  validates :completed, presence: true
 
   scope :owned_by, -> (user_id) { where user_id: user_id }
   scope :completed, -> { where completed: true }
